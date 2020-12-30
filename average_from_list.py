92e20b0f11170f2d9c9fe4e59cf1c18fb7776468
Andreas <andrewkontokanis+github@gmail.com>
2020-12-30 20:38:23 +0200

    Add files via upload

---
+++ average_from_list.py
@@ -0,0 +1,10 @@
+import random
+l=[random.randint(0,1000000) for i in range(50)]
+print(l)
+def mesos_oros(x):
+    sum=0
+    for i in x:
+        sum+=i
+    sum=sum/50
+    return sum
+print(mesos_oros(l))
