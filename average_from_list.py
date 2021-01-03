b200d140166ebaff800c6f9cf844b37f48c6a539
Andreas <andrewkontokanis+github@gmail.com>
2021-01-03 19:07:59 +0200

    Update average_from_list to be pep8 ready

--- average_from_list.py
+++ average_from_list.py
@@ -1,10 +1,14 @@
 import random
-l=[random.randint(0,1000000) for i in range(50)]
+l = [random.randint(0, 1000000) for i in range(50)]
 print(l)
+
+
 def mesos_oros(x):
-    sum=0
+    sum = 0
     for i in x:
-        sum+=i
-    sum=sum/50
+        sum += i
+    sum = sum/50
     return sum
+
+
 print(mesos_oros(l))
